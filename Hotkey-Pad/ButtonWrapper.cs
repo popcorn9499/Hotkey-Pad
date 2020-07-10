05f9761c1ae93e65dc02a9d238c85cbcb0cd40d0
popcorn <popcorn9499@gmail.com>
2020-07-09 19:34:20 -0500

    added font size support

--- Hotkey-Pad/ButtonWrapper.cs
+++ Hotkey-Pad/ButtonWrapper.cs
@@ -14,16 +14,17 @@ namespace hotkey_pad
         public Button thisButton = new Button();
 
 
-        public ButtonWrapper(int pointX, int pointY, int height, int width, string text, Color? backColor = null, Color? foreColor = null, string name="DynamicButton")
+        public ButtonWrapper(int pointX, int pointY, int height, int width, string text, Color? backColor = null, Color? foreColor = null,string font = "Microsoft Sans Serif", float fontSize = 8.25f, string name="DynamicButton")
         {
             if (backColor == null) backColor = Color.Transparent;
             if (foreColor == null) foreColor = Color.Black;
             
-            thisButton.Location = new Point(pointX,pointY);
-            thisButton.Height = height;
-            thisButton.Width = width;
-            thisButton.Text = text;
-            thisButton.Name = name;
+            this.thisButton.Location = new Point(pointX,pointY);
+            this.thisButton.Height = height;
+            this.thisButton.Width = width;
+            this.thisButton.Text = text;
+            this.thisButton.Name = name;
+            this.thisButton.Font = new Font(font, fontSize);
             this.thisButton.BackColor = Color.Transparent;
             this.thisButton.ForeColor = Color.Black ;
 
