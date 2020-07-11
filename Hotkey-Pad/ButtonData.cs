bcd308ff92d11da2d4afa140cf12421702683dc2
popcorn <popcorn9499@gmail.com>
2020-07-11 00:56:40 -0500

    created a place for button data

---
+++ Hotkey-Pad/ButtonData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotkey_Pad
+{
+    public class ButtonData
+    {
+        public string Connection { get; set; }
+        public string BtnText { get; set; }
+        public string cmdExeEnable { get; set; }
+        public string cmdExeCommand { get; set; }
+        public string hotkeyEnable { get; set; }
+        public string hotkeyCombo { get; set; }
+    }
+}
