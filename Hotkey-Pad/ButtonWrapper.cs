6148ab622479621874e2b2f2ad05704e0d641e44
popcorn <popcorn9499@gmail.com>
2020-07-11 02:27:14 -0500

    readded the button wrapper

--- Hotkey-Pad/ButtonWrapper.cs
+++ Hotkey-Pad/ButtonWrapper.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows;
+using System.Windows.Input;
 
 namespace Hotkey_Pad
 {
@@ -14,9 +15,19 @@ namespace Hotkey_Pad
 
         public Button thisButton = new Button();
 
+        public ButtonData buttonData;
 
-        public ButtonWrapper(int pointX, int pointY, int height, int width, string text, Color? backColor = null, Color? foreColor = null, string font = "Microsoft Sans Serif", float fontSize = 8.25f, string name = "DynamicButton")
+
+        public ButtonWrapper(ButtonData buttonData,string text, Thickness margin, Color? backColor = null, 
+            Color? foreColor = null, string font = "Microsoft Sans Serif", float fontSize = 8.25f, string name = "DynamicButton")
         {
+            this.buttonData = buttonData;
+
+            this.thisButton.Content = text;
+            this.thisButton.Name = name;
+            this.thisButton.Margin = margin;
+            this.thisButton.Cursor = Cursors.Arrow;
+            this.thisButton.Click += Button_Click;
             /*
             if (backColor == null) backColor = Color.FromArgb(0,0,0,0);
             if (foreColor == null) foreColor = Color.FromRgb(255,255,255);
@@ -24,7 +35,7 @@ namespace Hotkey_Pad
             this.thisButton.Location = new Point(pointX, pointY);
             this.thisButton.Height = height;
             this.thisButton.Width = width;
-            this.thisButton.Text = text;
+            this.thisButton.Content = text;
             this.thisButton.Name = name;
             this.thisButton.Font = new Font(font, fontSize);
             this.thisButton.BackColor = Color.Transparent;
@@ -32,7 +43,12 @@ namespace Hotkey_Pad
             */
         }
 
-
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("HI");
+            ButtonPadEditor x = new ButtonPadEditor(buttonData);
+            x.Show();
+        }
 
     }
 }
