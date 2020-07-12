398052e7bc2421b77ad6a3c29fc5854f438d96b0
popcorn <popcorn9499@gmail.com>
2020-07-12 01:21:49 -0500

    fixed a saving issue with json deserializing the object
    d'oh

--- Hotkey-Pad/Hotkey.cs
+++ Hotkey-Pad/Hotkey.cs
@@ -11,9 +11,9 @@ namespace Hotkey_Pad
 {
     public class Hotkey
     {
-        public Key key { get; }
+        public Key key { get; set; }
 
-        public ModifierKeys modifiers { get; }
+        public ModifierKeys modifiers { get; set; }
 
         public Hotkey() {}
 
@@ -67,7 +67,7 @@ namespace Hotkey_Pad
                 str.Append("Win + ");
 
             str.Append(key);
-            if (str == null) //cleans it up so that we dont just have a (None) String
+            if (str != null) //cleans it up so that we dont just have a (None) String
                 return str.ToString();
             return "";
         }
